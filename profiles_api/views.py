9a84b86da58c118edb420bc343ed99b27dfbb663
Kishan Jagadeesh <kishanjstlr@gmail.com>
2023-02-25 20:51:42 +0530

    first hello world api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        an_api_view=[
+            'Hare Krishna',
+            'Hare Krishna',
+            'Krishna Krishna',
+            'Hare Hare'
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_api_view})
